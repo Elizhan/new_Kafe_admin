6ba1ae74036e06a675ca08e5049c29c686efe693
elzantursunbaev@gmail.com <elzantursunbaev@gmail.com>
2024-07-09 12:03:51 +0600

    Добавьте файлы проекта.

---
+++ AdminKafe/Models/CafeName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminKafe.Models
+{
+    public class CafeName
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+}
